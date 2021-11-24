7302586648051d4200aff4f49163f505639119bd
adamisrusty <adamgutierreztx@gmail.com>
2021-11-24 15:52:30 -0500

    added sleep function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::{io, thread, time};
 
 fn main() {
+    print!("{esc}[2J{esc}[1:1H", esc = 27 as char);
     println!("Hello, world!");
+}
+
+fn sleep_seconds(secs: u64) {
+    thread::sleep(time::Duration::from_secs(secs));
 }
