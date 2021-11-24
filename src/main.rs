44fd57ddbaad5157e8e5cb562d16c31f8e335c96
adamisrusty <adamgutierreztx@gmail.com>
2021-11-24 16:09:36 -0500

    completed interaction

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,47 @@
 use std::{io, thread, time};
 
 fn main() {
+    let mut name = String::new();
     print!("{esc}[2J{esc}[1:1H", esc = 27 as char);
-    println!("Hello, world!");
+
+    println!(".");
+    sleep_seconds(1);
+    println!("..");
+    sleep_seconds(2);
+    println!("A wormhole and a GROM falls out.");
+    println!("'Who are you?', the GROM says.");
+    sleep_seconds(1);
+    println!("You say to the GROM:");
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read user input.");
+
+    let name = name.trim();
+
+    if name == "" {
+        sleep_seconds(1);
+        println!("The GROM syas 'Fine. Be that way.' and paddles out in to the metaverse.");
+        sleep_seconds(2);
+        println!("You spin in the GROM's wake and drift out into deepspace.");
+        sleep_seconds(1);
+        println!("end program.");
+    } else {
+        sleep_seconds(2);
+        println!(
+            "The GROM says 'Hello {}. Let's ride!' and tossed you a tow rope.",
+            name
+        );
+        sleep_seconds(1);
+        println!("The moment you touch the rope, time and space folds around you.");
+        sleep_seconds(1);
+        println!(".");
+        sleep_seconds(1);
+        println!("..");
+        sleep_seconds(1);
+        println!("Welcome to the metaverse.");
+    }
+    sleep_seconds(1);
 }
 
 fn sleep_seconds(secs: u64) {
